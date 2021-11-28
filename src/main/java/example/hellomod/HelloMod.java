96bc627b524fead268cc89331449b7c543f1ea41
Antti Ruotsalainen <antti.ruotsalainen@vuoma.fi>
2021-11-28 17:21:39 +0200

    Basic

--- src/main/java/example/hellomod/HelloMod.java
+++ src/main/java/example/hellomod/HelloMod.java
@@ -1,2 +1,15 @@
-public class HelloMod {
+package example.hellomod;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HelloMod implements ModInitializer {
+
+    public static final Logger LOGGER = LogManager.getLogger("modid");
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello fabirc world!");
+    }
 }
